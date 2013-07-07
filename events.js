aba5f21187506d89982668800b85ae6c5d005409
Daniel Nephin <dnephin@gmail.com>
2013-07-06 21:34:06 -0600

    Adding flicker.

--- events.js
+++ events.js
@@ -1,11 +1,27 @@
 
 
+no_op = function() {}
+
 Reveal.addEventListener('shock', function() {
 
     console.log('starting flicker')
     var ele = $('.flicker').removeClass('active')
 
-    setTimeout(function() {
-        ele.addClass('active');
-    }, 1000);
+    make_toggle = function(delay, follow) {
+        return function() {
+            ele.toggleClass('active');
+           setTimeout(follow, delay);
+        }
+    }
+
+    toggles = make_toggle(500, 
+              make_toggle(100,
+              make_toggle(100,
+              make_toggle(100,
+              make_toggle(100,
+              make_toggle(100,
+              make_toggle(0, no_op)))))))
+
+    setTimeout(toggles, 1200)
+
 })
